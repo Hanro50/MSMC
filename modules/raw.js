baf1a60eac2b72a50f67ba94a74fc6fc88e74cdf
Hanro <hlareman2@gmail.com>
2021-07-08 17:15:00 +0200

    Fixes with the "raw" gui framework
    Fixed a bug that caused the lib to not launch on Windows
    
    Fixed a crash that happened on slower machines
    
    Generally improved reliability

--- modules/raw.js
+++ modules/raw.js
@@ -2,12 +2,27 @@
  * EXPERIMENTAL!
  * Only compatible with Linux and Windows at the stage. 
  */
+const path = require('path')
 const fs = require('fs')
 const msmc = require('..');
-const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 const BE = require("./backEnd");
 const os = require("os");
-const temp = os.tmpdir() + "/msmc"; // /tmp
+const temp = path.join(os.tmpdir(), "msmc"); // /tmp
+const pref = path.join(temp, "Preferences")
+console.log(temp)
+
+async function setup() {
+    try {
+        if (fs.existsSync(pref)) {
+
+        }
+    } catch {
+        fs.writeFile(pref, "{'state': 0}", () => { console.log("Rewrote file extensions") })
+    }
+
+}
+setup();
 const defaultProperties = {
     width: 500,
     height: 650,
@@ -25,7 +40,7 @@ switch (type) {
         const locations = process.env.PATH.split(":");
         LE: {
             for (var i = 0; i < locations.length; i++) {
-                const compatible = [ "chromium", "google-chrome", "microsoft-edge", "brave-browser","vivaldi","blisk-browser","yandex-browser"]
+                const compatible = ["chromium", "google-chrome", "microsoft-edge", "vivaldi", "brave-browser", "blisk-browser", "yandex-browser"]
                 for (var i2 = 0; i2 < compatible.length; i2++) {
                     const s = locations[i] + "/" + compatible[i2];
                     if (fs.existsSync(s)) {
@@ -37,57 +52,68 @@ switch (type) {
             console.error("[MSMC]: No Chromium browser was found")
         }
 }
-module.exports = (token, updates = () => { }, Windowproperties = defaultProperties) => {
-    const cmd = Windowproperties.browserCMD ? Windowproperties.browserCMD : start;
-    if (!cmd) {
-        return Promise.reject("[MSMC] Error : no chromium browser was set, cannot continue!");
-    }
-    console.warn("[MSMC]: This setting is experimental");
-    console.warn("[MSMC]: Using \"" + cmd + "\"")
-    var redirect = msmc.createLink(token);
-    return new Promise(resolve => {
-        const f = exec(cmd + " --window-size=" + Windowproperties.width + "," + Windowproperties.height + " --remote-debugging-port=0 --no-first-run --no-default-browser-check --user-data-dir=" + temp + " --force-app-mode --app=\"" + redirect + "\"");
-        setTimeout(() => {
-            const data = fs.readFileSync(temp + "/DevToolsActivePort").toString();
-            const port = data.substr(0, data.indexOf("\n"));
-            const f3 = setInterval(() => {
-                BE.getFetch()("http://localhost:" + port + "/json").then(r => r.json()).then(out => {
-                    for (var i = 0; i < out.length; i++) {
-                        const loc = out[i].url;
-                        if (!loc || !loc.startsWith(token.redirect)) {
-                            if (loc && loc.startsWith("chrome-extension")) {
-                                //Thank you Blisk!
-                                BE.getFetch()("http://localhost:" + port + "/json/close/" + out[i].id)
+
+function browserLoop(token, port, updates, browser) {
+    return new Promise((resolve, reject) => {
+        const f3 = setInterval(() => {
+            BE.getFetch()("http://localhost:" + port + "/json/list").then(r => r.json()).then(out => {
+                for (var i = 0; i < out.length; i++) {
+                    const loc = out[i].url;
+                    if (loc && loc.startsWith(token.redirect)) {
+                        try {
+                            clearInterval(f3);
+                            for (var i2 = 0; i2 < out.length; i2++) {
+                                BE.getFetch()("http://localhost:" + port + "/json/close/" + out[i2].id)
                             }
-                            break;
+                            browser.kill("SIGILL")
+
+                        } catch {
+                            console.error("[MSMC] Failed to close window!");
                         }
                         const urlParams = new URLSearchParams(loc.substr(loc.indexOf("?") + 1)).get("code");
                         if (urlParams) {
                             resolve(msmc.authenticate(urlParams, token, updates));
                         } else {
                             updates({ type: "Cancelled" });
+                            reject("[MSMC] Action cancelled by user")
                         }
-                        try {
-                            clearInterval(f3);
-                            for (var i2 = 0; i2 < out.length; i2++) {
-                                BE.getFetch()("http://localhost:" + port + "/json/close/" + out[i2].id)
-                            }
-                            f.kill("SIGILL")
-
-                        } catch {
-                            console.error("[MSMC] Failed to close window!");
-                        }
-                        return true;
-
                     }
-                }).catch(err => {
-                    clearInterval(f3);
-                    f.kill();
-                    updates({ type: "Cancelled" });
-                    console.log(err)
-                })
-            }, 500);
-
+                }
+            }).catch(() => {
+                clearInterval(f3);
+                browser.kill();
+                updates({ type: "Cancelled" });
+                reject("[MSMC] Action cancelled by user")
+            })
         }, 500);
+
+    });
+}
+
+module.exports = (token, updates = () => { }, Windowproperties = defaultProperties) => {
+    const cmd = Windowproperties.browserCMD ? Windowproperties.browserCMD : start;
+    if (!cmd) {
+        return Promise.reject("[MSMC] Error : no chromium browser was set, cannot continue!");
+    }
+    console.warn("[MSMC]: This setting is experimental");
+    console.warn("[MSMC]: Using \"" + cmd + "\"");
+    var redirect = msmc.createLink(token);
+    return new Promise(resolve => {
+        const browser = spawn(cmd, ["--disable-component-extensions-with-background-pages", "--no-first-run", "--disable-extensions", "--window-size=" + Windowproperties.width + "," + Windowproperties.height, "--remote-debugging-port=0", "--no-default-browser-check", "--user-data-dir=" + temp, "--force-app-mode", "--app=" + redirect + ""]);
+        var firstrun = true;
+        const ouput = (out) => {
+            const cout = String(out.toString()).toLocaleLowerCase().trim();
+            //console.log(cout)
+            if (firstrun && cout.startsWith("devtools listening on ws://127.0.0.1:")) {
+                //console.log("exec")
+                firstrun = false;
+                var data = cout.substr("devtools listening on ws://127.0.0.1:".length);
+                const port = data.substr(0, data.indexOf("/"));
+                //console.log(out.toString())
+                resolve(browserLoop(token, port, updates, browser));
+            }
+        }
+        browser.stdout.on('data', ouput)
+        browser.stderr.on('data', ouput)
     });
 }
