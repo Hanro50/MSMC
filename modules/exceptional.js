86c0a4fc920ca71315d3c564a2f6f628851a69c6
Hanro <hlareman2@gmail.com>
2021-07-10 19:59:40 +0200

    Update exceptional.js

--- modules/exceptional.js
+++ modules/exceptional.js
@@ -3,7 +3,7 @@ const methods = ["authenticate", "refresh", "login", "fastLaunch", "launch"];
 methods.forEach(e => {
     module.exports[e] = async (...arguments) => {
         const result = await msmc[e](...arguments);
-        if (msmc.errorCheck) throw result;
+        if (msmc.errorCheck(result)) throw result;
         return result;
     }
 })
