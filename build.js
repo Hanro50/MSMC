1ab09d5e8a69ea79bb2d445da34e8c4d912e557b
Hanro <hlareman2@gmail.com>
2022-05-30 21:36:48 +0200

    Fixed build script

--- build.js
+++ build.js
@@ -2,15 +2,17 @@
 
 const { execSync } = require("child_process");
 const { rmSync } = require("fs");
+const {resolve } = require("path");
 
 rmSync('dist', { recursive: true, force: true });
 rmSync('types', { recursive: true, force: true });
-
 try {
+    console.log("[MSMC]:Checking dependencies....")
+    console.log(execSync(`npm i`).toString('ascii'))
     console.log("[MSMC]:Compiling....")
-    console.log(execSync('tsc -b ./tsconfig.dist.json').toString('ascii'))
+    console.log(execSync(`tsc -b ./tsconfig.dist.json`).toString('ascii'))
     console.log("[MSMC]:Constructing declarations....")
-    console.log(execSync('tsc -b ./tsconfig.types.json').toString('ascii'))
+    console.log(execSync(`tsc -b ./tsconfig.dist.json`).toString('ascii'))
 } catch (e) {
     console.log(e.toString('ascii'))
 }
