75f9482af6442df9c65053b59676e7c975846a65
Hanro <hlareman2@gmail.com>
2023-02-25 11:46:42 +0200

    Update wrapper.js

--- modules/wrapper.js
+++ modules/wrapper.js
@@ -4,15 +4,15 @@ const methods = [["authenticate", 2], ["refresh", 1], ["login", 2], ["launch", 2
 module.exports.exceptional = {};
 module.exports.callback = {};
 methods.forEach((method) => {
-    module.exports.exceptional[method[0]] = async (...arguments) => {
-        const result = await msmc[method[0]](...arguments);
+    module.exports.exceptional[method[0]] = async (...argz) => {
+        const result = await msmc[method[0]](...argz);
         if (msmc.errorCheck(result)) throw result;
         return result;
     }
-    module.exports.callback[method[0]] = (callback, ...arguments) => {
-        this.exceptional[method[0]](...arguments).then(callback).catch(err => {
-            if (arguments.length > method[1]) {
-                arguments[method[1]]({ type: "Error", error: err })
+    module.exports.callback[method[0]] = (callback, ...argz) => {
+        this.exceptional[method[0]](...argz).then(callback).catch(err => {
+            if (argz.length > method[1]) {
+                argz[method[1]]({ type: "Error", error: err })
             }
         });
     }
